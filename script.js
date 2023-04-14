0c2715b13642c0ee1be37a842287548c1fa6a2cb
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-04-14 19:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
-
+  str = str.split(" ").join("");
+  str = str.toLowerCase();
+  let start = 0,
+    end = str.length - 1,
+    flag = 0;
+  while (start < end) {
+    if (str.charAt(start++) != str.charAt(end--)) {
+      flag = 1;
+      break;
+    }
+  }
+  if (flag == 0) {
+    return true;
+  } else return false;
 }
 module.exports = palindrome
